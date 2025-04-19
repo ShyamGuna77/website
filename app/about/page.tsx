b9794023cdddbd738aefb361084678f082a5c16f
Shyam <gshyamprasad2001@gmail.com>
2025-04-19 22:44:38 +0530

    Writing blogs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@
 import {type Metadata } from "next"
 import Image from "next/image"
 
-
+import Reveal from "../utils/Reveal";
 import { Container } from "../components/Container"
 import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
@@ -41,71 +41,93 @@ const About = () => {
             </div>
           </div>
           <div className="lg:order-first lg:row-span-2">
-            <h1 className="text-4xl font-bold tracking-tight">
-              Hey, I’m Shyam Guna — a 21-year-old Full Stack Developer from
-              India with a passion for building clean, creative, and scalable
-              digital experiences.
-            </h1>
+            <Reveal>
+              <h1 className="text-4xl font-bold tracking-tight">
+                Hey, I’m Shyam Guna — a 21-year-old Full Stack Developer from
+                India with a passion for building clean, creative, and scalable
+                digital experiences.
+              </h1>
+            </Reveal>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-              <p>
-                Despite Graduating from a RoadSide college with Limited
-                resources.I turned my constaints into oppurtunities. I was
-                building my own curriculum through online courses,Youtube,
-                open-source projects,Blogs, and countless nights debugging code
-                until sunrise.
-              </p>
-              <p>
-                My learning playground? Twitter , GitHub repos, and Discord
-                Convos. I stay on top of the latest tech trends by being active
-                in online dev communities, constantly soaking up insights and
-                experimenting with new tools. I love building sleek frontends,
-                robust backends, and anything in between that turns ideas into
-                real, usable products.
-              </p>
-              <p>
-                Outside of Coding . I am an Anime enthusiast(Otaku alert!) and
-                Film Lover. My anime collection ranges from the intricate
-                mysteries of Death Note to the epic world-building stories of
-                One Piece, where I find inspiration in Luffy's persistence and
-                creativity when facing seemingly impossible obstacles.
-              </p>
-              <p>
-                Into anime and coding? Same here! Whether you're passionate
-                about building cool stuff or binge-watching the latest season of
-                your favorite anime, let's connect. I’m always up for tech
-                chats, debugging sessions, or dropping anime recommendations.
-                Hit me up — let’s geek out together!
-              </p>
+              <Reveal>
+                <p>
+                  Despite Graduating from a RoadSide college with Limited
+                  resources.I turned my constaints into oppurtunities. I was
+                  building my own curriculum through online courses,Youtube,
+                  open-source projects,Blogs, and countless nights debugging
+                  code until sunrise.
+                </p>
+              </Reveal>
+              <Reveal>
+                <p>
+                  My learning playground? Twitter , GitHub repos, and Discord
+                  Convos. I stay on top of the latest tech trends by being
+                  active in online dev communities, constantly soaking up
+                  insights and experimenting with new tools. I love building
+                  sleek frontends, robust backends, and anything in between that
+                  turns ideas into real, usable products.
+                </p>
+              </Reveal>
+              <Reveal>
+                <p>
+                  Outside of Coding . I am an Anime enthusiast(Otaku alert!) and
+                  Film Lover. My anime collection ranges from the intricate
+                  mysteries of Death Note to the epic world-building stories of
+                  One Piece, where I find inspiration in Luffy's persistence and
+                  creativity when facing seemingly impossible obstacles.
+                </p>
+              </Reveal>
+              <Reveal>
+                <p>
+                  Into anime and coding? Same here! Whether you're passionate
+                  about building cool stuff or binge-watching the latest season
+                  of your favorite anime, let's connect. I’m always up for tech
+                  chats, debugging sessions, or dropping anime recommendations.
+                  Hit me up — let’s geek out together!
+                </p>
+              </Reveal>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list" className="space-y-5 hover:text-blue-500">
-              <SocialText href="https://x.com/Hikki_7d" icon={FaXTwitter}>
-                Follow on X
-              </SocialText>
-              <SocialText
-                href="https://www.instagram.com/shyamguna77/"
-                icon={FaInstagram}
-              >
-                Follow on Instagram
-              </SocialText>
-              <SocialText
-                href="https://www.linkedin.com/in/shyamguna77/"
-                icon={FaLinkedin}
-              >
-                Follow on Linkedn
-              </SocialText>
-              <SocialText href="https://github.com/ShyamGuna77" icon={FaGithub}>
-                Follow on Github
-              </SocialText>
-
-              <SocialText
-                href="mailto:shyamprasad8247@gmail.com"
-                icon={MdMail}
-                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
-              >
-                shyamprasad8247@gmail.com
-              </SocialText>
+              <Reveal>
+                <SocialText href="https://x.com/Hikki_7d" icon={FaXTwitter}>
+                  Follow on X
+                </SocialText>
+              </Reveal>
+              <Reveal>
+                <SocialText
+                  href="https://www.instagram.com/shyamguna77/"
+                  icon={FaInstagram}
+                >
+                  Follow on Instagram
+                </SocialText>
+              </Reveal>
+              <Reveal>
+                <SocialText
+                  href="https://www.linkedin.com/in/shyamguna77/"
+                  icon={FaLinkedin}
+                >
+                  Follow on Linkedn
+                </SocialText>
+              </Reveal>
+              <Reveal>
+                <SocialText
+                  href="https://github.com/ShyamGuna77"
+                  icon={FaGithub}
+                >
+                  Follow on Github
+                </SocialText>
+              </Reveal>
+              <Reveal>
+                <SocialText
+                  href="mailto:shyamprasad8247@gmail.com"
+                  icon={MdMail}
+                  className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+                >
+                  shyamprasad8247@gmail.com
+                </SocialText>
+              </Reveal>
             </ul>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,58 @@
-import Image from "next/image";
-import { Sun } from "lucide-react";
+/* eslint-disable react/no-unescaped-entities */
+
 import { Container } from "./components/Container";
 import { SocialLink } from "./components/SocialIcon";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import Reveal from "./utils/Reveal";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-9">
         <div className="max-w-2xl">
-          <h1
-            className="text-4xl font-bold tracking-tight text-zinc-800
+          <Reveal>
+            <h1
+              className="text-4xl font-bold tracking-tight text-zinc-800
           sm:text-5xl dark:text-zinc-100"
-          >
-            Frontend, backend, anime weekend
-          </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I'm Shyam, a fullstack developer and tech enthusiast from India. I
-            craft digital experiences with websockets, animation with Framer
-            Motion, bringing both backend logic and frontend magic to life
-          </p>
+            >
+              Frontend, backend, anime weekend
+            </h1>
+          </Reveal>
+          <Reveal>
+            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+              I'm Shyam, a fullstack developer and tech enthusiast from India. I
+              craft digital experiences with websockets, animation with Framer
+              Motion, bringing both backend logic and frontend magic to life
+            </p>
+          </Reveal>
         </div>
-        <div className="mt-6 flex gap-6">
-          <SocialLink
-            href="https://www.instagram.com/shyamguna77/"
-            aria-label="Follow on Instagram"
-            icon={FaInstagram}
-          />
-          <SocialLink
-            href="https://www.linkedin.com/in/shyamguna77/"
-            aria-label="Follow on Linkedin"
-            icon={FaLinkedin}
-          />
-          <SocialLink
-            href="https://github.com/ShyamGuna77"
-            aria-label="Follow on Github"
-            icon={FaGithub}
-          />
-          <SocialLink
-            href="https://x.com/Hikki_7d"
-            aria-label="Follow on X"
-            icon={FaXTwitter}
-          />
-        </div>
+        <Reveal>
+          <div className="mt-6 flex gap-6">
+            <SocialLink
+              href="https://www.instagram.com/shyamguna77/"
+              aria-label="Follow on Instagram"
+              icon={FaInstagram}
+            />
+            <SocialLink
+              href="https://www.linkedin.com/in/shyamguna77/"
+              aria-label="Follow on Linkedin"
+              icon={FaLinkedin}
+            />
+            <SocialLink
+              href="https://github.com/ShyamGuna77"
+              aria-label="Follow on Github"
+              icon={FaGithub}
+            />
+            <SocialLink
+              href="https://x.com/Hikki_7d"
+              aria-label="Follow on X"
+              icon={FaXTwitter}
+            />
+          </div>
+        </Reveal>
       </Container>
     </>
   );
