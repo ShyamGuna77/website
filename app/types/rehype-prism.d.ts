dbeb6412b4cbd48040bac5aa22708e62527c8f20
Shyam <gshyamprasad2001@gmail.com>
2025-04-19 08:49:37 +0530

    feat:added articles

---
+++ app/types/rehype-prism.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "@mapbox/rehype-prism" {
+  const rehypePrism: any;
+  export default rehypePrism;
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+import rehypePrism from '@mapbox/rehype-prism'
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/articles/*': ['./src/app/articles/**/*.mdx'],
+    },
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
