61dcb338d8a134a7acd83b7a19895a3cd5f42233
Shyam <gshyamprasad2001@gmail.com>
2025-04-20 08:43:15 +0530

    added framere motion

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -4,25 +4,35 @@ import { SimpleLayout } from '../components/SimpleLayout'
 import { formatDate } from '@/lib/formatDate'
 import { type ArticleWithSlug,getAllArticles } from '@/lib/articles'
 import { Metadata } from 'next'
+import Reveal from '../utils/Reveal'
 
 function Article({ article }: { article: ArticleWithSlug }) {
   return (
     <article className="md:grid md:grid-cols-4 md:items-baseline">
       <Card className="md:col-span-3">
-        <Card.Title href={`/articles/${article.slug}`}>
-          {article.title}
-        </Card.Title>
-        <Card.Eyebrow
-          as="time"
-          dateTime={article.date}
-          className="md:hidden"
-          decorate
-        >
-          {formatDate(article.date)}
-        </Card.Eyebrow>
-        <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
+        <Reveal>
+          <Card.Title href={`/articles/${article.slug}`}>
+            {article.title}
+          </Card.Title>
+        </Reveal>
+        <Reveal>
+          <Card.Eyebrow
+            as="time"
+            dateTime={article.date}
+            className="md:hidden"
+            decorate
+          >
+            {formatDate(article.date)}
+          </Card.Eyebrow>
+        </Reveal>
+        <Reveal>
+          <Card.Description>{article.description}</Card.Description>
+        </Reveal>
+        <Reveal>
+          <Card.Cta>Read article</Card.Cta>
+        </Reveal>
       </Card>
+     
       <Card.Eyebrow
         as="time"
         dateTime={article.date}
@@ -30,6 +40,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
       >
         {formatDate(article.date)}
       </Card.Eyebrow>
+   
     </article>
   );
 }
@@ -44,18 +55,21 @@ export default async function ArticlesIndex() {
   const articles = await getAllArticles();
 
   return (
-    <SimpleLayout
-      title="Just a Dev Who loves to write Blogs about Tech,Movies..."
-      intro="A collection of my long-form thoughts — on code, system design, anime, tech culture, and everything I’m learning along the way. All in the order they happened."
-    >
-      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-        <div className="flex max-w-3xl flex-col space-y-16">
-          {articles.map((article) => (
-            <Article key={article.slug} article={article} />
-          ))}
-        </div>
-      </div>
-    </SimpleLayout>
+
+   
+        <SimpleLayout
+          title="Just a Dev Who loves to write Blogs about Tech,Movies..."
+          intro="A collection of my long-form thoughts — on code, system design, anime, tech culture, and everything I’m learning along the way. All in the order they happened."
+        >
+          <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+            <div className="flex max-w-3xl flex-col space-y-16">
+              {articles.map((article) => (
+                <Article key={article.slug} article={article} />
+              ))}
+            </div>
+          </div>
+        </SimpleLayout>
+  
   );
 }
 
