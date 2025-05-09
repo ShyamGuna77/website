aad62c67571c7fd94ca8a74799c5744dbd996873
Shyam <gshyamprasad2001@gmail.com>
2025-05-09 07:55:31 +0530

    fixed article button

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Card } from '../components/Card'
-import { SimpleLayout } from '../components/SimpleLayout'
-import { formatDate } from '@/lib/formatDate'
-import { type ArticleWithSlug,getAllArticles } from '@/lib/articles'
-import { Metadata } from 'next'
-import Reveal from '../utils/Reveal'
+import React from "react";
+import { Card } from "../components/Card";
+import { SimpleLayout } from "../components/SimpleLayout";
+import { formatDate } from "@/lib/formatDate";
+import { type ArticleWithSlug, getAllArticles } from "@/lib/articles";
+import { Metadata } from "next";
+import Reveal from "../utils/Reveal";
 
 function Article({ article }: { article: ArticleWithSlug }) {
   return (
@@ -29,10 +29,12 @@ function Article({ article }: { article: ArticleWithSlug }) {
           <Card.Description>{article.description}</Card.Description>
         </Reveal>
         <Reveal>
-          <Card.Cta>Read article</Card.Cta>
+          <Card.Link href={`/articles/${article.slug}`}>
+            <Card.Cta>Read article</Card.Cta>
+          </Card.Link>
         </Reveal>
       </Card>
-     
+
       <Card.Eyebrow
         as="time"
         dateTime={article.date}
@@ -40,7 +42,6 @@ function Article({ article }: { article: ArticleWithSlug }) {
       >
         {formatDate(article.date)}
       </Card.Eyebrow>
-   
     </article>
   );
 }
@@ -50,26 +51,21 @@ export const metadata: Metadata = {
     "All of my long-form thoughts on programming, anime, product,Prodcasts and more, collected in chronological order.",
 };
 
-
 export default async function ArticlesIndex() {
   const articles = await getAllArticles();
 
   return (
-
-   
-        <SimpleLayout
-          title="Just a Dev Who loves to write Blogs about Tech,Movies..."
-          intro="A collection of my long-form thoughts — on code, system design, anime, tech culture, and everything I’m learning along the way. All in the order they happened."
-        >
-          <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-            <div className="flex max-w-3xl flex-col space-y-16">
-              {articles.map((article) => (
-                <Article key={article.slug} article={article} />
-              ))}
-            </div>
-          </div>
-        </SimpleLayout>
-  
+    <SimpleLayout
+      title="Just a Dev Who loves to write Blogs about Tech,Movies..."
+      intro="A collection of my long-form thoughts — on code, system design, anime, tech culture, and everything I'm learning along the way. All in the order they happened."
+    >
+      <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="flex max-w-3xl flex-col space-y-16">
+          {articles.map((article) => (
+            <Article key={article.slug} article={article} />
+          ))}
+        </div>
+      </div>
+    </SimpleLayout>
   );
 }
-
